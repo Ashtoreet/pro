b0b45a2aa678c6cd0289f161b3111cf1de6c410b
Ashtoreet <Ashtoreet@icloud.com>
2018-11-27 22:22:50 +0300

    refactoring service_selenium

--- service_selenium.py
+++ service_selenium.py
@@ -7,18 +7,18 @@ import time
 
 # html = 'https://service.nalog.ru/uwsfind.do'
 # html = 'https://service.nalog.ru/bi.do'
-inn = # [*, *, *, *, *, *, *, *, *, *]
-driver = webdriver.Chrome()
+inn = []
+# driver = webdriver.Chrome()
+driver = webdriver.PhantomJS()
+
 html = 'https://service.nalog.ru/zd.do'
 
 
 def input_inn_to_form(driver, inn):
     """ Input inn from variable. """
-    input_inn = driver.find_element_by_id('inn') # find inn form
+    input_inn = driver.find_element_by_id('inn')  # find inn form
 
-    for i in inn:
-        driver.implicitly_wait(5) # timer
-        input_inn.send_keys(i) # input one by one
+    [input_inn.send_keys(i) for i in inn]  # input one by one
 
 
 def open_img(driver):
@@ -27,6 +27,7 @@ def open_img(driver):
     responce = requests.get(img_url)
     img = Image.open(BytesIO(responce.content))
     img.show()
+    return img
 
 def enter_cap(driver):
     """ тут работает ввод капчи с консоли """
@@ -34,9 +35,7 @@ def enter_cap(driver):
     list_user_input = list(map(int,"".join(map(str, user_input)))) # turn to list
     input_captcha = driver.find_element_by_id('captcha') # find form
 
-    for c in list_user_input:
-        driver.implicitly_wait(4) #timer
-        input_captcha.send_keys(c) # input one by one
+    [input_captcha.send_keys(c) for c in list_user_input] # input one by one
 
 
 def click_button(driver):
@@ -48,18 +47,15 @@ def getting_output(driver):
     value = driver.find_element_by_id('resultData').text.strip()
     msg = driver.find_element_by_id('resultData').get_attribute('outerHTML')
 
-    if value:
-        print(value)
-    else:
-        print('пусто', msg)
+    print(value) if value else print('пусто', msg)
 
 
 if __name__ == '__main__':
     driver.get(html)
-    driver.implicitly_wait(15) # это таймер
+    driver.implicitly_wait(15) # timer
 
     input_inn_to_form(driver, inn)
-    open_img(driver)
+    img = open_img(driver)
     enter_cap(driver)
     click_button(driver)
 
@@ -67,3 +63,4 @@ if __name__ == '__main__':
 
     getting_output(driver)
     driver.close()
+    # img.close() not work
